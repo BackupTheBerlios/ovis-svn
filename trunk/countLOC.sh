1fa214e323435ff1951fa1e685df32b56d68c5e2
pforai <pforai@4f864796-9004-0410-bb7e-9f6182938f85>
2005-11-09 00:30:31 +0000

    added script that counts LOC.

---
+++ trunk/countLOC.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# it should be evident what's this script purpose. 
+
+echo "========================================================================"
+echo "total lines of code in .cpp files: "
+ find . -name *.cpp -exec cat {} \; | wc -l
+echo "total lines of code in .hh files: " 
+ find . -name *.hh -exec cat {} \; | wc -l
+echo "========================================================================"
